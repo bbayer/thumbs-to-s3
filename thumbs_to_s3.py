e0615d95ef7bd3c52b002c91a4921c9f24dfe394
bbayer <bbayer82@gmail.com>
2012-08-29 19:15:05 +0300

    some changes regarding S3 directory structure and file naming.

--- thumbs_to_s3.py
+++ thumbs_to_s3.py
@@ -8,7 +8,7 @@ https://github.com/bbayer
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 from optparse import OptionParser
-from os.path import isfile,basename
+from os.path import isfile,basename,isdir,dirname,exists
 from urlparse import urlparse
 
 import re
@@ -19,6 +19,7 @@ import sys
 import unicodedata
 import json
 import Image
+import os
 
 def slugify(value):
   """ Normalizes string, converts to lowercase, removes non-alpha characters,
@@ -35,17 +36,17 @@ def create_s3_conn(key, secret, bucket):
   bucket = conn.get_bucket(bucket)
   return conn, bucket
 
-def upload_file( filepath, bucket):
+def upload_file( src_filepath, remote_filepath, bucket ):
   """ Uploads file with filepath to a bucket. Bucket shall be boto.s3.bucket object
   Uploaded file has key name as its filename and it will be publicly accessible
   """
   try:
     k = Key(bucket)
-    k.key = basename(filepath)
-    k.set_contents_from_filename(filepath)
+    k.key = remote_filepath
+    k.set_contents_from_filename(src_filepath)
     k.set_acl('public-read')
   except Exception,e:
-    print e
+    sys.stderr.write("An error occured while uploading file:%s" % src_filepath)
     return None
   return k
 
@@ -67,18 +68,26 @@ def print_upload_data(opts, data):
       sys.exit(3)
     
   
-def create_thumbnail( infile, outfile, size ):
+def create_thumbnail( local_filename, remote_filename, temp_folder, size ):
   """ Creates a thumbnail from infile and save it to outfile with specified size.
   Size must be a tuple with (width,height)
   It will terminate execution if something went wrong. Yes I am too lazy to implement full error flow control.
   """
   try:
-    im = Image.open(infile)
-    im.thumbnail(size,Image.ANTIALIAS)
-    im.save(outfile,"JPEG")
-    return im.size
+    im = Image.open(local_filename)
+    if size[0] != 0 or size[1] != 0:
+      im.thumbnail(size,Image.ANTIALIAS)
+    dirs, filename = os.path.split(remote_filename)
+    dirs = os.path.join( temp_folder, dirs )
+    try:
+      os.mkdirs(dirs)
+    except Exception,e:
+      pass
+    dest_filepath = os.path.join(temp_folder, remote_filename)
+    im.save(dest_filepath,"JPEG")
+    return ( dest_filepath, im.size[0], im.size[1])
   except IOError,e:
-    sys.stderr.write("An error occured while creating thumbnail:%s",e)
+    sys.stderr.write("An error occured while creating thumbnail:%s" % e)
     sys.exit(3)
   
 def options():
@@ -90,9 +99,9 @@ def options():
   parser.add_option("-s", "--secret",
                     dest="secret", help="AWS Secret")
   parser.add_option("-b", "--bucket",
-                    dest="bucket", help="S3 bucket name")
-  parser.add_option("-u", "--upload-original",
-                    dest="upload_original", action="store_true", default=False, help="Upload original file to S3")
+                    dest="bucket", help="S3 bucket name") 
+  parser.add_option("-f", "--temp-folder",
+                    dest="temp_folder", help="Temporary folder for files",default="/temp")
   parser.add_option("-t", "--thumb-size",
                     dest="thumbnails", action="append", type="string", help="Size of thumbnail. It should be form of WxH\
                     (Note that resize always preserves aspect ratio.)")
@@ -119,17 +128,20 @@ def options():
     
   if options.thumbnails != None:
     for t in  options.thumbnails:
-      if re.search(r'^\d+x\d+$',t) == None:
-        parser.error("Thumbnail size specification should be in WxH format. :%s" % t)
+      if re.search(r'^\d+x\d+:[\w\$\.\/]+$',t) == None:
+        parser.error("Thumbnail size specification should be in WxH:filename_for_output format. :%s" % t)
   return (options,args[0])
 
 def get_filename_from_url(url):
-  """ Creates a filename from specified url """
-  
+  """ Creates a filename from specified url """ 
   parsed_url = urlparse(urllib2.unquote(url))
   retval = basename(parsed_url.path)
   return retval
-  
+
+def purge_extension(filename):
+  filename = basename(filename)
+  return filename[:filename.rfind(".")]
+
 def main():
   """ Program entry point. Here is what is going on
   1. Check CL arguments.
@@ -140,11 +152,11 @@ def main():
   6. Print information about files in specified format.
   """
   (opts,path) = options()
-  real_path = None
+  src_filepath = None
   uploaded_files = []
   #Determine argument is file or url
   if isfile(path):
-    real_path = path
+    src_filepath = path
   else:
     #If url specified download to local
     try:
@@ -152,46 +164,51 @@ def main():
     except:
       sys.stderr.write("An error occured while downloading image file: %s\n" % path)
       sys.exit(2)
-    tmpfile = open(get_filename_from_url(path),"w+b");
-    real_path = tmpfile.name
+
+    tmpfilename = get_filename_from_url(path)
+    tmpfile = open(tmpfilename,"w+b");
+    src_filepath = tmpfile.name
     tmpfile.write(f.read())
     tmpfile.close()
-  
+ 
+  #Create temp folder if not exists
+  if not isdir(opts.temp_folder) and not exists(opts.temp_folder):
+    try:
+      os.mkdirs(opts.temp_folder)
+    except Exception,e:
+      sys.stderr.write("Temp folder can not be created: %s\n" % opts.temp_folder);
+      sys.exit(3)
+
   urlbase = "https://s3.amazonaws.com/%s/" % opts.bucket
   #Check whether file is correct
-  if real_path == None:
-    sys.stderr.write("Specified file couldn't be found: %s\n" %path);
+  if src_filepath == None:
+    sys.stderr.write("Specified file couldn't be found: %s\n" % path);
     sys.exit(3);
 
   #Create a connection object to S3. If an error occured terminate.
   try:
-    conn, bucket = create_s3_conn(opts.key,opts.secret,opts.bucket)  
+    conn, bucket = create_s3_conn(opts.key, opts.secret, opts.bucket)
   except:
     sys.stderr.write("An error occured while uploading files to S3. Check your key/secret and bucket name")
     sys.exit(3);
   
-  #Upload original if it is specified from command line
-  if opts.upload_original:
-    im = Image.open(real_path)
-    data = { 'width':im.size[0], 
-             'height':im.size[1],
-             'filename':basename(real_path),
-             'url':urlbase+basename(real_path) }
-    if( upload_file( real_path, bucket ) != None):
-      uploaded_files.append(data)
     
   #If there is parameters specified, create thumbnails accordingly
   if opts.thumbnails != None:
+    basefilename = purge_extension(src_filepath)
     for t in opts.thumbnails:
       #Parse size parameters WxH => (w,h)
-      size = tuple([ int(x) for x in t.split("x")])
-      tmpfile = "thumb-%s-%d-%d.jpg" % ( slugify(basename(real_path)), size[0], size[1] )
-      actual_size = create_thumbnail(real_path, tmpfile, size)
-      data = { 'width': actual_size[0],
-               'height': actual_size[1],
-               'filename': tmpfile,
-               'url':urlbase+tmpfile }
-      if( upload_file( tmpfile, bucket ) != None):
+      matches = re.findall(r"^(\d+)x(\d+):([\w\$\.\/]+)$",t)
+      if len(matches) == 0:
+        continue;
+      width,height,format = matches[0]
+      remote_filepath = format.replace("$",slugify(basefilename))
+      local_thumbpath, real_width, real_height = create_thumbnail(src_filepath, remote_filepath, opts.temp_folder, (width,height) )
+      data = { 'width' : real_width,
+               'height' : real_height,
+               'filename' : remote_filepath,
+               'url' : urlbase + remote_filepath }
+      if( upload_file( local_thumbpath, remote_filepath, bucket ) != None):
         uploaded_files.append(data)
   
   #Finally print or send information of successfully uploaded files
